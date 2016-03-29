0f6e2e810b2cb77e390464d88912271a8773e629
erynjones <eryn.jones16@hotmail.com>
2016-03-29 17:07:40 -0400

    Collapse/Expand Added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var $btn = $('.btn-show-hide');
 var $move= $('.btn-move');
 var $bounce= $('.btn-bounce');
+var $collapse= $('.btn-collapse-expand');
 
 $btn.on('click', function (e) {
   $('.box').toggleClass('box-show');
@@ -8,6 +9,10 @@ $btn.on('click', function (e) {
 
 $move.on('click', function (e) {
   $('.diamond').toggleClass('diamond-move');
+});
+
+$collapse.on('click', function (e) {
+  $('.panel').toggleClass('expand');
 });
 
 $bounce.on('click', function (e) {
