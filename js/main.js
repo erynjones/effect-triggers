59575c11843270431b0585cddfaf28aa696eafec
erynjones <eryn.jones16@hotmail.com>
2016-03-29 16:55:27 -0400

    Move in/out transition added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 var $btn = $('.btn-show-hide');
+var $move= $('.btn-move');
+var $bounce= $('.btn-bounce');
 
 $btn.on('click', function (e) {
   $('.box').toggleClass('box-show');
+});
+
+$move.on('click', function (e) {
+  $('.diamond').toggleClass('diamond-move');
+});
+
+$bounce.on('click', function (e) {
+  $('.circle').toggleClass('bounce');
 });
